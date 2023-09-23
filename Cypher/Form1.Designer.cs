b885d48e66a7f20cd86cd87a9508b700ce5c6111
Kingry <Kingry@DESKTOP-ANITIV2>
2023-09-23 00:56:40 -0400

    Add project files.

---
+++ Cypher/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Cypher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadRawImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadEncodedImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveEncodedImageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(93, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(419, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 240);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(93, 285);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(646, 165);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Encode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Decode";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.saveEncodedImageToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(750, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadRawImageToolStripMenuItem,
+            this.loadEncodedImageToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadRawImageToolStripMenuItem
+            // 
+            this.loadRawImageToolStripMenuItem.Name = "loadRawImageToolStripMenuItem";
+            this.loadRawImageToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.loadRawImageToolStripMenuItem.Text = "Load Raw Image";
+            this.loadRawImageToolStripMenuItem.Click += new System.EventHandler(this.loadRawImageToolStripMenuItem_Click);
+            // 
+            // loadEncodedImageToolStripMenuItem
+            // 
+            this.loadEncodedImageToolStripMenuItem.Name = "loadEncodedImageToolStripMenuItem";
+            this.loadEncodedImageToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.loadEncodedImageToolStripMenuItem.Text = "Load Encoded Image";
+            this.loadEncodedImageToolStripMenuItem.Click += new System.EventHandler(this.loadEncodedImageToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(182, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // saveEncodedImageToolStripMenuItem
+            // 
+            this.saveEncodedImageToolStripMenuItem.Name = "saveEncodedImageToolStripMenuItem";
+            this.saveEncodedImageToolStripMenuItem.Size = new System.Drawing.Size(128, 20);
+            this.saveEncodedImageToolStripMenuItem.Text = "Save encoded image";
+            this.saveEncodedImageToolStripMenuItem.Click += new System.EventHandler(this.saveEncodedImageToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Keyword:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "alpha";            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(750, 462);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Image Encoder and Decoder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadRawImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadEncodedImageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem saveEncodedImageToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Cypher/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Cypher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bitmap;
+        Bitmap encodedImage;        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog1.Filter = "Image Files (*.png;*.jpg)|*.png;*.jpg";
+            openFileDialog1.FilterIndex = 1;
+
+            if (pictureBox1.Image == null)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+            }            
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "") button1.Enabled = false;
+            else button1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text != "" && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                byte[] messageBytes = Encoding.UTF8.GetBytes(richTextBox1.Text);
+                int maxBytes = ((bitmap.Width * bitmap.Height) - 32) / 8;
+                                
+                if (messageBytes.Length > maxBytes - 4)
+                {
+                    throw new InvalidOperationException($"Message is too long to encode in this image. Max bytes: {maxBytes - 4}");
+                }
+                encodedImage = ImageSteganography.EncodeMessage(bitmap, richTextBox1.Text, textBox1.Text);
+                pictureBox2.Image = encodedImage;
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                richTextBox1.Clear();
+                string message = ImageSteganography.DecodeMessage(encodedImage, textBox1.Text);
+                richTextBox1.Text = message;
+            }
+        }
+
+        private void loadRawImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                bitmap = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = bitmap;
+                if (richTextBox1.Text == "")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                }
+                else
+                {
+                    button1.Enabled = true;
+                    button2.Enabled = false;
+                }
+            }
+        }
+
+        private void loadEncodedImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            richTextBox1.Clear();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                encodedImage = new Bitmap(openFileDialog1.FileName);
+                pictureBox2.Image = encodedImage;
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+        }
+
+        private void saveEncodedImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox2.Image != null)
+            {
+                string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "outputImage.png");
+                encodedImage.Save(filePath, ImageFormat.Png);
+                MessageBox.Show("Saved " + filePath);
+            }
+        }
+    }
+    public static class ImageSteganography
+    {
+        public static string VigenereEncrypt(string text, string keyword)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                char keyChar = keyword[i % keyword.Length];
+                char encryptedChar = (char)((c + keyChar) % 256);
+                result.Append(encryptedChar);
+            }
+            return result.ToString();
+        }
+
+        public static string VigenereDecrypt(string text, string keyword)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                char keyChar = keyword[i % keyword.Length];
+                char decryptedChar = (char)((c - keyChar + 256) % 256);
+                result.Append(decryptedChar);
+            }
+            return result.ToString();
+        }
+
+        public static Bitmap EncodeMessage(Bitmap image, string message, string key)
+        {
+            string encryptedMessage = VigenereEncrypt(message, key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(encryptedMessage);
+            byte[] lengthBytes = BitConverter.GetBytes(messageBytes.Length);
+
+            for (int i = 0; i < 32; i++)
+            {
+                int x = i % image.Width;
+                int y = i / image.Width;
+
+                Color pixel = image.GetPixel(x, y);
+
+                int byteIndex = i / 8;
+                int bitIndex = i % 8;
+
+                int bit = (lengthBytes[byteIndex] >> bitIndex) & 1;
+                int newPixelValue = (pixel.ToArgb() & ~1) | bit;
+
+                image.SetPixel(x, y, Color.FromArgb(newPixelValue));
+            }
+
+            for (int i = 32; i < (messageBytes.Length * 8) + 32; i++)
+            {
+                int x = i % image.Width;
+                int y = i / image.Width;
+
+                Color pixel = image.GetPixel(x, y);
+
+                int byteIndex = (i - 32) / 8;
+                int bitIndex = (i - 32) % 8;
+
+                int bit = (messageBytes[byteIndex] >> bitIndex) & 1;
+                int newPixelValue = (pixel.ToArgb() & ~1) | bit;
+
+                image.SetPixel(x, y, Color.FromArgb(newPixelValue));
+            }
+
+            return image;
+        }
+
+
+        public static string DecodeMessage(Bitmap image, string key)
+        {
+            byte[] lengthBytes = new byte[4];
+            int bitCount = 0;
+
+            for (int i = 0; i < 32; i++)
+            {
+                int x = i % image.Width;
+                int y = i / image.Width;
+
+                int pixelValue = image.GetPixel(x, y).ToArgb();
+                int bit = pixelValue & 1;
+
+                int byteIndex = bitCount / 8;
+                int bitIndex = bitCount % 8;
+
+                lengthBytes[byteIndex] |= (byte)(bit << bitIndex);
+
+                bitCount++;
+            }
+
+            int messageLength = BitConverter.ToInt32(lengthBytes, 0);
+
+            if (messageLength <= 0)
+            {
+                return "Invalid Message Length";
+            }
+
+            byte[] messageBytes = new byte[messageLength];
+            bitCount = 0;
+
+            for (int i = 32; i < 32 + (messageLength * 8); i++)
+            {
+                int x = i % image.Width;
+                int y = i / image.Width;
+
+                int pixelValue = image.GetPixel(x, y).ToArgb();
+                int bit = pixelValue & 1;
+
+                int byteIndex = bitCount / 8;
+                int bitIndex = bitCount % 8;
+
+                messageBytes[byteIndex] |= (byte)(bit << bitIndex);
+
+                bitCount++;
+            }
+
+            string encryptedMessage = Encoding.UTF8.GetString(messageBytes);
+            string decryptedMessage = VigenereDecrypt(encryptedMessage, key);
+
+            return decryptedMessage;
+        }
+
+
+    }
+
+}
